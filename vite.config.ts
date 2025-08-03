fe5f5e7856c5130dc376c4497fbc5bd32bad076d
K K <45881661-korsunme@users.noreply.replit.com>
2025-08-03 10:36:05 +0000

    chore: update to use replit

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   server: {
     port: 3000,
+    allowedHosts: ['53f8c2e8-77bc-4033-9ed7-7cdddf159c6d-00-3jpycbzkd1mdu.worf.replit.dev'],
   },
   plugins: [react(), tailwindcss()],
 });
